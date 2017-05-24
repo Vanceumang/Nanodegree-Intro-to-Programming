2988138bbebe7026bed1507520178c13f3ea1f3f
vanimal37 <vmang37@gmail.com>
2017-05-23 22:33:54 -0400

    Add files via upload

--- mindstorms.py
+++ mindstorms.py
@@ -9,9 +9,13 @@ import turtle
 
 def draw_square():
     window = turtle.Screen()
-    window.bgcolor("red")
+    window.bgcolor("white")
     
     brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("blue")
+    brad.speed(.005)
+    
     brad.forward(100)
     brad.right(90)
     brad.forward(100)
@@ -20,6 +24,11 @@ def draw_square():
     brad.right(90)
     brad.forward(100)
     brad.right(90)
+    
+    van = turtle.Turtle()
+    van.shape("arrow")
+    van.color("red")
+    van.circle(100)
     
     window. exitonclick()
 draw_square()
