1d3a74672e88b9ea46ab652383c924c3562283fb
vanimal37 <vmang37@gmail.com>
2017-05-23 20:20:07 -0400

    Add files via upload

---
+++ mindstorms.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 23 19:34:37 2017
+
+@author: vmang
+"""
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("red")
+    
+    brad = turtle.Turtle()
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    
+    window. exitonclick()
+draw_square()
+
+
